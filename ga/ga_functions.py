a80b8eb590027a7799f21d1c8e76103d4100f26b
Bhavika <bk2002@hw.ac.uk>
2022-11-27 03:37:29 +0400

    completed mutation

--- ga/ga_functions.py
+++ ga/ga_functions.py
@@ -27,8 +27,62 @@ class GA:
             # of the objective function for dims number of times
             individual = [round(random.uniform(self.min_bound, self.max_bound),4) for _ in range(self.dims)]
             self.population.append(individual)
-        print("population:", self.population)
+        #print("population:", self.population)
 
+
+
+    """ TOURNAMENT SELECTION """
+    def tournament_selection(self, population, k = 3):
+
+        # calculating function values of each member of population
+        # it contains the values returned by the objective function
+        values = [self.function(i) for i in population]
+
+        # saving best individual incase deleted by mistake
+        best_index = np.argmin(values)
+        best_individual = population[best_index]
+
+        newPopulation = []
+        #print("values:", values)
+
+        for i in range(self.npop):
+            # extracting random indexes
+            random_index = np.random.choice(self.npop, k)
+            
+            best = random_index[0]
+            # finding best index from k number of random indexes (tournament)
+            for j in random_index:
+                if values[best] > values[j]:
+                    best = j
+
+            # inserting best individual from k random instances of population
+            newPopulation.append(population[best])
+        
+        # we want to make sure we do not lose the lowest value incase 
+        if best_individual not in newPopulation:
+            print("true")
+            newPopulation[-1] = best_individual
+        #print("new fit pop:", newPopulation)
+        return newPopulation
+
+
+
+    """ MUTATION """
+    def mutation(self, population, mr):
+
+        # if len(population[0]) <= 1:
+        #     raise ValueError("No mutation can occur for individuals with 1 or less variables")
+        # iterating through the whole population
+        for i in population:
+            # iterating through each individual
+            for j in range(len(i)):
+                # if random number is below the mutation rate, it falls under the probability
+                if mr > random.random():
+                    # shuffle in place, acts as mutation
+                    random.shuffle(i)
+
+        #print("mutation pop:", population)
+        return population
 
 
 
@@ -49,34 +103,7 @@ class GA:
                 newPopulation[i] = population[i][:cp] + population[i+1][cp:]
                 newPopulation[i+1] = population[i+1][:cp] + population[i][cp:]
 
-        print("new cross pop:", newPopulation)
-        return newPopulation
-
-
-
-    """ TOURNAMENT SELECTION """
-    def tournament_selection(self, population, k = 3):
-
-        # calculating fitness of each member of population
-        # it contains the values returned by the objective function
-        fitness = [self.function(i) for i in population]
-        newPopulation = []
-        print("fitness:", fitness)
-
-        for i in range(self.npop):
-            # extracting random indexes
-            random_index = np.random.choice(self.npop, k)
-            
-            best = random_index[0]
-            # finding best index from k number of random indexes (tournament)
-            for j in random_index:
-                if fitness[best] > fitness[j]:
-                    best = j
-
-            # inserting best individual from k random instances of population
-            newPopulation.append(population[best])
-        
-        print("new fit pop:", newPopulation)
+        #print("new cross pop:", newPopulation)
         return newPopulation
     
 
@@ -84,3 +111,4 @@ test = cec2005.F1(1)
 ga = GA(test, -100, 100, 1, 10, 10)
 ga.tournament_selection(ga.population, 3)
 ga.crossover(ga.population, 0.9)
+ga.mutation(ga.population, 0.5)
