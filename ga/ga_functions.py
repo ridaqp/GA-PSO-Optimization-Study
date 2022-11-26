fd26aaab2f7c58ecf489777cb158e91e6177fc12
Bhavika <bk2002@hw.ac.uk>
2022-11-26 19:12:21 +0400

    update

--- ga/ga_functions.py
+++ ga/ga_functions.py
@@ -6,6 +6,9 @@ class GA:
 
     def __init__(self, function, min_bound, max_bound, dims, pop, gen):
         self.function = function
+        self.min_bound = min_bound
+        self.max_bound = max_bound
+        self.dims = dims
         
         self.npop = pop
         self.gen = gen
@@ -13,5 +16,6 @@ class GA:
         self.population = []
         
         for i in range(self.npop):
-            individual = [round(random.uniform(min_bound, max_bound),4) for _ in range(dims)]
+            individual = [round(random.uniform(self.min_bound, self.max_bound),4) for _ in range(self.dims)]
             self.population.append(individual)
+        
