dcee66a871420fa7c93eecddaaae381715cfc526
Bhavika <bk2002@hw.ac.uk>
2022-11-26 23:05:52 +0400

    crossover works

--- ga/ga_functions.py
+++ ga/ga_functions.py
@@ -29,15 +29,40 @@ class GA:
             self.population.append(individual)
         print("population:", self.population)
 
+
+
+
+    """ ONE-POINT CROSSOVER """
+    def crossover(self, population, cr):
+        # by default, crossover is copy of previous gen parents
+        newPopulation = population.copy()
+
+        # forming parent pairs in population
+        for i in range(0, self.npop, 2):
+            # checking probability of crossover
+            # if random number is below the crossover rate, it falls under the probability
+            if cr > random.random():
+                # finding crossover point within length of parent (dims)
+                cp = random.randint(0, self.dims - 1)
+                # performing crossover, creating 2 new children by merging variables from parents
+                # to or from crossover point
+                newPopulation[i] = population[i][:cp] + population[i+1][cp:]
+                newPopulation[i+1] = population[i+1][:cp] + population[i][cp:]
+
+        print("new cross pop:", newPopulation)
+        return newPopulation
+
+
+
     """ TOURNAMENT SELECTION """
     def tournament_selection(self, population, k = 3):
 
         # calculating fitness of each member of population
         # it contains the values returned by the objective function
         fitness = [self.function(i) for i in population]
-        newpopulation = []
-
+        newPopulation = []
         print("fitness:", fitness)
+
         for i in range(self.npop):
             # extracting random indexes
             random_index = np.random.choice(self.npop, k)
@@ -49,15 +74,13 @@ class GA:
                     best = j
 
             # inserting best individual from k random instances of population
-            newpopulation.append(population[best])
+            newPopulation.append(population[best])
         
-        print("new pop:", newpopulation)
-        return newpopulation
+        print("new fit pop:", newPopulation)
+        return newPopulation
     
-
-
-
 
 test = cec2005.F1(1)
 ga = GA(test, -100, 100, 1, 10, 10)
 ga.tournament_selection(ga.population, 3)
+ga.crossover(ga.population, 0.9)
