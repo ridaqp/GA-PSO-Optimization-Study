696f8465a15781d30364624fa235707f16c85e29
Bhavika <bk2002@hw.ac.uk>
2022-11-26 22:27:12 +0400

    function works

--- ga/ga_functions.py
+++ ga/ga_functions.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+# for testing
+from optproblems import cec2005
+
 class GA:
 
     def __init__(self, function, min_bound, max_bound, dims, pop, gen):
@@ -9,7 +12,7 @@ class GA:
         self.function = function
         self.min_bound = min_bound # min int value for function
         self.max_bound = max_bound # max int value for function
-        self.dims = dims # dimension of each input to objective function
+        self.dims = dims # number of variables of each input to objective function
 
         # population number
         self.npop = pop
@@ -24,7 +27,7 @@ class GA:
             # of the objective function for dims number of times
             individual = [round(random.uniform(self.min_bound, self.max_bound),4) for _ in range(self.dims)]
             self.population.append(individual)
-
+        print("population:", self.population)
 
     """ TOURNAMENT SELECTION """
     def tournament_selection(self, population, k = 3):
@@ -34,6 +37,7 @@ class GA:
         fitness = [self.function(i) for i in population]
         newpopulation = []
 
+        print("fitness:", fitness)
         for i in range(self.npop):
             # extracting random indexes
             random_index = np.random.choice(self.npop, k)
@@ -41,10 +45,19 @@ class GA:
             best = random_index[0]
             # finding best index from k number of random indexes (tournament)
             for j in random_index:
-                if fitness[j] > fitness[best]:
+                if fitness[best] > fitness[j]:
                     best = j
 
             # inserting best individual from k random instances of population
-            newpopulation.append[population[best]]
+            newpopulation.append(population[best])
         
+        print("new pop:", newpopulation)
         return newpopulation
+    
+
+
+
+
+test = cec2005.F1(1)
+ga = GA(test, -100, 100, 1, 10, 10)
+ga.tournament_selection(ga.population, 3)
