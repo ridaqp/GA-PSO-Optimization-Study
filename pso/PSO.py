85e14c4fce16f30d993f07733b3f5415c3ebea63
Rida <rida.aqp@gmail.com>
2022-11-26 22:47:33 +0400

    initialising PSO class struct

---
+++ pso/PSO.py
@@ -0,0 +1,16 @@
+import particle
+
+class PSO(): 
+
+    def __init__(self):
+        dimensions = 0
+        dimensions_min = []
+        dimensions_max = []
+        alphalimit = 0
+        betalimit = 0
+        maxIters = 0
+        gbest = []
+
+
+
+
