99b1df211da1a94e5732fcc63a84ae01d58239a5
Rida <rida.aqp@gmail.com>
2022-11-26 23:27:04 +0400

    updating pso implementation

--- pso/PSO.py
+++ pso/PSO.py
@@ -2,7 +2,7 @@ import particle
 
 class PSO(): 
 
-    def __init__(self):
+    def __init__(self, benchmark):
         dimensions = 0
         dimensions_min = []
         dimensions_max = []
@@ -10,6 +10,17 @@ class PSO():
         betalimit = 0
         maxIters = 0
         gbest = []
+        fitnessfunc = benchmark
+        swarm = []
+
+    def evaluate_swarm(self):
+        pass
+
+    def update_swarm(self): 
+        pass 
+
+    
+
 
 
 
