4fc870ea7f050bb3a4a6a735c906c98e90d24694
Rida <rida.aqp@gmail.com>
2022-11-27 00:01:27 +0400

    updates to PSO implementation

--- pso/PSO.py
+++ pso/PSO.py
@@ -3,15 +3,20 @@ import particle
 class PSO(): 
 
     def __init__(self, benchmark):
-        dimensions = 0
-        dimensions_min = []
-        dimensions_max = []
-        alphalimit = 0
-        betalimit = 0
+
+        dimensions = 0  # dimensions of the search space
+      
+        # set upper limits on different hyperparameters
+        c1limit = 0 # for personal best
+        c2limit = 0 # for social best
+        c3limit = 0 # for global best
+
         maxIters = 0
-        gbest = []
+        
+        gbest = []  # to keep track of the global best solutions in every iteration
         fitnessfunc = benchmark
         swarm = []
+        step = 0.1  # step size for position update
 
     def evaluate_swarm(self):
         pass
@@ -19,7 +24,17 @@ class PSO():
     def update_swarm(self): 
         pass 
 
-    
+    """Evaluate and update particles until convergence"""
+    def optimise(self, benchmark):
+        
+        for i in range (self.maxIters):
+            self.evaluate_swarm()
+            self.update_swarm()
+            
+            #if converged:
+                #break out of loop
+
+
 
 
 
