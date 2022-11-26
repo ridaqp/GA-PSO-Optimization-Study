237f83f3bfb8259832b00e764efe93c9fdd20187
Rida <rida.aqp@gmail.com>
2022-11-26 23:27:04 +0400

    updating pso implementation

--- pso/particle.py
+++ pso/particle.py
@@ -1,13 +1,23 @@
+
 class particle(): 
-    def __init__(self):
-        self.position = []
-        self.velocity = []
-        self.pbest = []
-        self.gbest = []
-        self.dims = []
-        self.inertia = 0
-        self.alpha = 0
-        self.beta = 0
+    def __init__(self, dims, size):
+
+        self.swarmsize = size #no. of particles in the swarm/population
+
+        self.position = []   # current particle's position
+        self.velocity = []   # current particle's velocity
+        self.informants = [] # current particle's neighbours
+
+        # current best positions
+        self.infbest = self # the best position among neighbours
+        self.pbest = self   # the particle's best
+        self.gbest = self   # the swarm's best
+
+        # hyperparameters
+        self.inertia = 0 # weight for previous velocity
+        self.alpha = 0  # acceleration coefficient for personal best
+        self.beta = 0   # acc. coeff. for informants best
+        self.gamma = 0  # acc. coeff. for global best
 
     def evaluate(self):
         pass
