2a5a91f48196411dc0726b7f3840999158859218
Rida <rida.aqp@gmail.com>
2022-11-29 01:47:07 +0400

    unstable results

--- pso/PSO.py
+++ pso/PSO.py
@@ -24,22 +24,6 @@ class PSO():
         self.gbest = float('inf')
         self.gbest_pos = np.zeros(dims)
         self.iters = iters
-        # self.dimensions = dims  # dimensions of the search space
-        # self.swarmsize = size #no. of particles in the swarm/population
-
-        # self.swarm = self.setParticles()
-
-        # # set upper limits on different hyperparameters
-        # alp = 0 # for personal best
-        # c2limit = 0 # for social best
-        # c3limit = 0 # for global best
-
-        # maxIters = iters
-
-        # gbest = []  # to keep track of the global best solutions in every iteration
-        # fitnessfunc = benchmark
-        
-        # step = 0.7  # step size for position update
 
     def evaluate_swarm(self):
         for i in range (self.iters):
@@ -50,21 +34,10 @@ class PSO():
                 self.gbest_pos, self.gbest = particle.updatePos(self.lbound, self.ubound, self.gbest_pos, self.gbest)
         return self.gbest_pos, self.gbest
 
-
-    # """Evaluate and update particles until convergence"""
-    # def optimise(self, benchmark):
-        
-    #     for i in range (self.maxIters):
-    #         self.evaluate_swarm()
-    #         self.update_swarm()
-            
-    #         #if converged:
-    #             #break out of loop
-
 #Test: 
 benchmark = cec2005.F1(10)
 # create pso 
-swarm = PSO(benchmark, -100, 100, 1, 10, 10, 80)
+swarm = PSO(benchmark, -100, 100, 0.4, 10, 10, 100)
 best, value = swarm.evaluate_swarm()
 print(" the best search position is", best, "whose values is", value)
 
